b6d110dd8ea9abe0c27c295f9161591cf34b898d
qonitha95 <qonitha95@student.uns.ac.id>
2021-03-07 17:53:22 +0700

    Template dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/admin', function () {
+    return view('layouts.admin.template');
+});
