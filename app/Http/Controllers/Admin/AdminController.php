602f2c297f9ddea25a52bdb5b4c46c6a7bb1d273
luthfi_puji1 <luthfi_puji1@student.uns.ac.id>
2021-03-09 19:32:08 +0700

    rev admincontroller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
-//use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
