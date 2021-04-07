0fd16b9658182e3ec0caf9fe10e95c603690ac76
qonitha95 <qonitha95@gmail.com>
2021-04-07 20:37:42 +0700

    Resolve konflik

--- resources/views/pages/admin/home.blade.php
+++ resources/views/pages/admin/home.blade.php
@@ -3,14 +3,9 @@
 @section('title', 'Dashboard')
 
 @section('container')
-<<<<<<< HEAD
-<!-- Main content -->
-<section class="content">
-=======
     <!-- Main content -->
     <section class="content">
         <div class="row mt-3">
->>>>>>> d886bb0f960433fef990856be42f8efe09bd6138
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
@@ -20,11 +15,7 @@
               <div class="inner">
                 <h3>150</h3>
 
-<<<<<<< HEAD
-                <p>Orders</p>
-=======
                 <p>New Orders</p>
->>>>>>> d886bb0f960433fef990856be42f8efe09bd6138
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
@@ -35,32 +26,6 @@
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
-<<<<<<< HEAD
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>44</h3>
-
-                <p>User Registrations</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="/users" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                <p>Clients</p>
-              </div>
-              <div class="icon">
-                <i class="fas fa-user-secret"></i>
-=======
             <div class="small-box bg-success">
               <div class="inner">
                 <h3>53<sup style="font-size: 20px">%</sup></h3>
@@ -84,33 +49,21 @@
               </div>
               <div class="icon">
                 <i class="ion ion-person-add"></i>
->>>>>>> d886bb0f960433fef990856be42f8efe09bd6138
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
-<<<<<<< HEAD
-          
-=======
           <!-- ./col -->
->>>>>>> d886bb0f960433fef990856be42f8efe09bd6138
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-danger">
               <div class="inner">
                 <h3>65</h3>
 
-<<<<<<< HEAD
-                <p>Translators</p>
-              </div>
-              <div class="icon">
-                <i class="fas fa-user-tie"></i>
-=======
                 <p>Unique Visitors</p>
               </div>
               <div class="icon">
                 <i class="ion ion-pie-graph"></i>
->>>>>>> d886bb0f960433fef990856be42f8efe09bd6138
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
@@ -118,22 +71,11 @@
           <!-- ./col -->
         </div>
         <!-- /.row -->
-<<<<<<< HEAD
-        <!-- Main row -->
-        <div class="row">
-          <!-- Left col -->
-          <section class="col-lg-7 connectedSortable">
-          </section>    
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-=======
        
         <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
         </div>
     </section>
     <!-- /.content -->
->>>>>>> d886bb0f960433fef990856be42f8efe09bd6138
 
 @endsection
