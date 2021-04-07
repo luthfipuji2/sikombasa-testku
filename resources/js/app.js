e7d9dd8ff89eb3ef41615670d34ffa62306e2430
qonitha95 <qonitha95@gmail.com>
2021-04-07 22:11:25 +0700

    Konflik

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,4 @@ require('./bootstrap');
 require('@fortawesome/fontawesome-free/js/all.js');
 require('admin-lte');
 
-<<<<<<< HEAD
 require('alpinejs');
-
-
-=======
-require('alpinejs');
->>>>>>> 6de14fe088de7ca4b5ead3be09a71e8fe690a2f7
