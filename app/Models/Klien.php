354ef2741603a67c14c702b1782da528a9baf1ed
luthfi_puji1 <luthfi_puji1@student.uns.ac.id>
2021-04-09 09:52:10 +0700

    tambah model, migration, dan order

---
+++ app/Models/Klien.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Order as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+
+class Klien extends Model
+{
+    protected $table = 'klien';
+    protected $primaryKey = 'id_klien';
+
+    protected $fillable = [
+        'id',
+        'nik',
+        'alamat',
+        'provinsi',
+        'kabupaten',
+        'kecamatan',
+        'kode_pos',
+        'tgl_lahir',
+        'jenis_kelamin',
+        'no_telp',
+        'foto_ktp'
+    ];
+
+    public function order(){
+        return $this->hasMany('App\Order');
+    }
+}
