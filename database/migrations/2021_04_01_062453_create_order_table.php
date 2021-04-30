1b10a1b2183ac904b8f86edcccc8cf97b00d115c
luthfi_puji1 <luthfi_puji1@student.uns.ac.id>
2021-04-30 07:14:43 +0700

    rev tabel order

--- database/migrations/2021_04_01_062453_create_order_table.php
+++ database/migrations/2021_04_01_062453_create_order_table.php
@@ -25,7 +25,7 @@ class CreateOrderTable extends Migration
             $table->string('jumlah_halaman')->nullable();
             $table->string('text')->nullable();
             $table->string('nama_dokumen')->nullable();
-            $table->string('upload_dpkumen')->nullable();
+            $table->string('upload_dokumen')->nullable();
             $table->string('path_file')->nullable();
             $table->string('size')->nullable();
             $table->string('ekstensi')->nullable();
